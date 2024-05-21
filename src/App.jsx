ea2a7aa739eab297c1653a543f894ca48520eccb
catalina-to24 <catalinatempraotero@gmail.com>
2024-05-20 22:49:53 -0300

    preparandonos para el parcial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Card from './Card'
+import Form from './Form'
+
+function App() {
+
+  return (
+    <>
+      <Form />
+      <Card />
+      
+    </>
+  )
+}
+
+export default App
